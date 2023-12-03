05e4a677e3a95122f2a0d06e28d81bb2f03c5764
No One <tqdatqn01230@gmail.com>
2023-12-03 15:58:13 +0700

    Initial commit

---
+++ src/fpt/mantv/Main.java
@@ -0,0 +1,16 @@
+package fpt.mantv;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.SequenceInputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+
+}
